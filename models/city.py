8b410f0ee053ab18178d7d82744cff521b24981e
Dani-1138 <danieldemeke92@gmail.com>
2023-05-14 23:30:41 -0700

    model

---
+++ models/city.py
@@ -0,0 +1,12 @@
+""" holds class City"""
+from models.base_model import BaseModel
+
+
+class City(BaseModel):
+    """Representation of city """
+    state_id = ""
+    name = ""
+
+    def __init__(self, *args, **kwargs):
+        """initializes city"""
+        super().__init__(*args, **kwargs)
